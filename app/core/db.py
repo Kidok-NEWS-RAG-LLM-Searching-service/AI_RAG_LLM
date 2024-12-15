9737d1dec5cf110bdaacb28a025f930d49aad3d2
oereo <dlstpgns0406@gmail.com>
2024-12-15 15:17:19 +0900

    feat: dynamodb에서 mongodb로 변경

--- app/core/db.py
+++ app/core/db.py
@@ -1,12 +1,26 @@
 import boto3
+import certifi
 from boto3.resources.base import ServiceResource
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from app.core.config import settings
 
 
-log_db: ServiceResource = boto3.resource(
-        'dynamodb',
-        region_name=settings.log_db_region_name,
-        aws_access_key_id=settings.log_db_access_key_id,
-        aws_secret_access_key=settings.log_db_secret_access_key
-    )
+class MongoDB:
+    def __init__(self):
+        self.client = None
+
+    def connect(self):
+        self.client = AsyncIOMotorClient(settings.mongo_db_url,tlsCAFile=certifi.where())
+        self.db = self.client[settings.mongo_db_name]
+        print("DB 와 연결되었습니다.")
+
+    def close(self):
+        self.client.close()
+
+    def get_db(self):
+        return self.db
+
+
+mongodb = MongoDB()
+mongodb.connect()
