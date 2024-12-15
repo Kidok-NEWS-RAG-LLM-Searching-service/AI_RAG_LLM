9b2790cab373c5feaeda5d7736633185ad9f9b64
oereo <dlstpgns0406@gmail.com>
2024-12-15 15:20:58 +0900

    feat: search keyword, answer, like, like with feedback에 대한 4가지 타입 로그 데이터 적재 method 정의

--- app/api/service/logs/log.py
+++ app/api/service/logs/log.py
@@ -2,19 +2,36 @@ from app.api.repository.ai_search_log_repository import ai_search_log_repository
 from app.api.repository.ai_search_log_type import AISearchLogType
 
 
-@DeprecationWarning
-def read():
-    items = ai_search_log_repository.get_all()
-    print("items", items)
-
-
-@DeprecationWarning
-def put():
-    ai_search_log_repository.put_item(
+async def put_search_tracking(query: str):
+    await ai_search_log_repository.put_item(
         log_type=AISearchLogType.SEARCH,
-        query="test",
+        query=query,
     )
 
 
-def get_item(column_name, value):
-    return ai_search_log_repository.get_by_column(column_name=column_name, value=value)
+async def put_search_response_tracking(query: str, answer: str):
+    await ai_search_log_repository.put_item(
+        log_type=AISearchLogType.ANSWER,
+        query=query,
+        answer=answer
+    )
+
+
+async def put_search_response_like_tracking(query: str, answer: str, like: int):
+    await ai_search_log_repository.put_item(
+        log_type=AISearchLogType.LIKE_BUTTON_ONLY,
+        query=query,
+        answer=answer,
+        like=like
+    )
+
+
+async def put_search_response_like_with_feedback_tracking(query: str, answer: str, like: int, feedback: str):
+    await ai_search_log_repository.put_item(
+        log_type=AISearchLogType.DESCRIPTION,
+        query=query,
+        answer=answer,
+        like=like,
+        description=feedback
+    )
+
