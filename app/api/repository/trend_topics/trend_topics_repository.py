14cc95da65c7c355602f329d9f87b6ff0e5819fe
SamuelOhO <ohyongsock@gmail.com>
2025-03-28 23:31:56 +0900

    fix: get trendtopics 오늘날짜 데이터 없으면 어제 데이터 긁어오도록 설정

--- app/api/repository/trend_topics/trend_topics_repository.py
+++ app/api/repository/trend_topics/trend_topics_repository.py
@@ -1,6 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from app.core.db import mongodb
+from datetime import datetime, timedelta
 
 
 class TrendTopicRepository:
@@ -8,14 +9,19 @@ class TrendTopicRepository:
     def __init__(self, db: AsyncIOMotorDatabase):
         self.__collection = db["trend_topic"]
 
-    async def find_today_trend_topics(self, today_date: str):
-        cursor = self.__collection.find({"date": today_date}).sort([
-            ("date", 1),
-            ("order", 1)
-        ])
-        trend_topics_in_collection = await cursor.to_list(length=None)
-        print(len(trend_topics_in_collection))
-        trend_topics = []
+    async def find_today_trend_topics(self, today_date: datetime):
+        trend_topics_in_collection = []
+        date = today_date
+        
+        while not trend_topics_in_collection:
+            cursor = self.__collection.find({"date": str(date)}).sort([
+                ("date", 1),
+                ("order", 1)
+            ])
+            trend_topics_in_collection = await cursor.to_list(length=None)
+            print(len(trend_topics_in_collection))
+            trend_topics = []
+            date -= timedelta(days=1)
 
         for trend_topic in trend_topics_in_collection:
             trend_topics.append({
